8375a9e41555711983241ea235bbef0389d0b5d7
Skymefly <TheSaraSparkle@gmail.com>
2015-08-25 11:37:52 -0700

    Played around

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,20 @@
+int x = mouseX;
+int y = mouseY;
+
 void setup()
 {
-
+background(255,141,218);
+size(400,400);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+stroke(255,255,255);
+ellipse(200, 200, 50, 50);
 }
+
+
+
+
+
+
 
---
+++ build-tmp/source/OriginalDesign.java
@@ -0,0 +1,45 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
+int x = mouseX;
+int y = mouseY;
+
+public void setup()
+{
+background(255,141,218);
+size(400,400);
+}
+public void draw()
+{
+stroke(255,255,255);
+ellipse(200, 200, 50, 50);
+}
+
+
+
+
+
+
+
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
