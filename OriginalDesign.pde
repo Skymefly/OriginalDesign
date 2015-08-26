07ea025812883b7bfb5edfb861db072eaeab0e61
Skymefly <TheSaraSparkle@gmail.com>
2015-08-26 11:39:24 -0700

    Added circles

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,15 +1,26 @@
-int x = mouseX;
-int y = mouseY;
+int x = 1;
+int r = 10;
+int b = 10;
+int g = 10;
 
 void setup()
 {
 background(255,141,218);
 size(400,400);
 }
+
 void draw()
 {
-stroke(255,255,255);
-ellipse(200, 200, 50, 50);
+	stroke(255,255,255);
+	fill(r,g,b);
+	ellipse((int)(Math.random()*400),(int)(Math.random()*400),50,50);
+	x=x+2;
+	if (x == 300001) {
+		background(r, b, g);
+		r=r+10;
+		g=g+10;
+		b=b+10;
+	}
 }
 
 
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,18 +14,29 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-int x = mouseX;
-int y = mouseY;
+int x = 1;
+int r = 10;
+int b = 10;
+int g = 10;
 
 public void setup()
 {
 background(255,141,218);
 size(400,400);
 }
+
 public void draw()
 {
-stroke(255,255,255);
-ellipse(200, 200, 50, 50);
+	stroke(255,255,255);
+	fill(r,g,b);
+	ellipse((int)(Math.random()*400),(int)(Math.random()*400),50,50);
+	x=x+2;
+	if (x == 300001) {
+		background(r, b, g);
+		r=r+10;
+		g=g+10;
+		b=b+10;
+	}
 }
 
 
