e2c996228956978dc6bb3ca206b1fcf2bc82ca9d
Skymefly <TheSaraSparkle@gmail.com>
2015-08-27 11:37:01 -0700

    Added lines

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,29 +1,35 @@
 int x = 1;
-int r = 10;
-int b = 10;
-int g = 10;
-
 void setup()
 {
-background(255,141,218);
+background(255,255,255);
 size(400,400);
+frameRate(5000);
 }
 
 void draw()
 {
+	int r = (int)(Math.random()*256);
+	int b = (int)(Math.random()*256);
+	int g = (int)(Math.random()*256);
 	stroke(255,255,255);
 	fill(r,g,b);
 	ellipse((int)(Math.random()*400),(int)(Math.random()*400),50,50);
+	fill(255,255,255);
+	ellipse(mouseX, mouseY, 50,50);
 	x=x+2;
-	if (x == 300001) {
-		background(r, b, g);
-		r=r+10;
-		g=g+10;
-		b=b+10;
+	if (x > 30000) {
+			lines();
 	}
 }
 
-
+void lines()
+{
+	int r = (int)(Math.random()*256);
+	int b = (int)(Math.random()*256);
+	int g = (int)(Math.random()*256);
+	stroke(r,g,b);
+	line((int)(Math.random()*400),(int)(Math.random()*400),(int)(Math.random()*400),(int)(Math.random()*400));
+}
 
 
 
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -15,31 +15,37 @@ import java.io.IOException;
 public class OriginalDesign extends PApplet {
 
 int x = 1;
-int r = 10;
-int b = 10;
-int g = 10;
-
 public void setup()
 {
-background(255,141,218);
+background(255,255,255);
 size(400,400);
+frameRate(5000);
 }
 
 public void draw()
 {
+	int r = (int)(Math.random()*256);
+	int b = (int)(Math.random()*256);
+	int g = (int)(Math.random()*256);
 	stroke(255,255,255);
 	fill(r,g,b);
 	ellipse((int)(Math.random()*400),(int)(Math.random()*400),50,50);
+	fill(255,255,255);
+	ellipse(mouseX, mouseY, 50,50);
 	x=x+2;
-	if (x == 300001) {
-		background(r, b, g);
-		r=r+10;
-		g=g+10;
-		b=b+10;
+	if (x > 30000) {
+			lines();
 	}
 }
 
-
+public void lines()
+{
+	int r = (int)(Math.random()*256);
+	int b = (int)(Math.random()*256);
+	int g = (int)(Math.random()*256);
+	stroke(r,g,b);
+	line((int)(Math.random()*400),(int)(Math.random()*400),(int)(Math.random()*400),(int)(Math.random()*400));
+}
 
 
 
